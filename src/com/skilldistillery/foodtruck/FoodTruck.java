bead78fb8f46a89eb5d2019c4c2fbe8040c5a555
Daniel.Kregstein <Akregstein@hotmail.com>
2022-02-26 17:42:15 -0700

    testing commit before completion

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -6,17 +6,29 @@ public class FoodTruck {
 	private String food;
 	private int rating;
 	private int numericId;
-	private static int  nextNumericId = 0;
+	private static int nextNumericId = 0;
+
+	public String toString() {
+		String output = "Truck name: " + name + ", Food type: " + food + ", User Rating: " + rating + ", Truck Numeric Id: " + numericId;
+				
+		return output;
+	}
+	
+	public void truckInfo() {
+		String truckInfo = this.toString();
+		System.out.println(truckInfo);
+	}
+		
 	public FoodTruck() {
 
 	}
 
-	public FoodTruck(String name, String food, int rating ) {
-		this.name= name;
-		this.food =food;
+	public FoodTruck(String name, String food, int rating) {
+		this.name = name;
+		this.food = food;
 		this.rating = rating;
-		this.numericId=  nextNumericId ;
-		nextNumericId = nextNumericId++;
+		this.numericId = nextNumericId;
+		 nextNumericId++;
 	}
 
 	public String getName() {
@@ -31,21 +43,27 @@ public class FoodTruck {
 		return food;
 	}
 
-	public void  setFood(String food) {
-	this.name = food;
+	public void setFood(String food) {
+		this.name = food;
 	}
-	
+
 	public int getRating() {
 		return rating;
 	}
+
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
+
 	public int numericId() {
 		return numericId;
 	}
+
 	public void numericId(int numericId) {
-	this.numericId= numericId;
+		this.numericId = numericId;
 	}
-}
-	
+
+
+
+	}
+
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -6,6 +6,7 @@ public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
+		boolean menu = true;
 		
 		FoodTruck[]  foodTrucks = new FoodTruck[5];
 		
@@ -34,14 +35,22 @@ public class FoodTruckApp {
 			
 		}
 			
-		
-			System.out.println("Please choose a numeric selection from menu");
+		while(menu) {
+			System.out.println();
+			System.out.println("**********************************");
+			System.out.println("*** Please choose a numeric selection from menu ***");
 			System.out.println("1. List all food trucks");
 			System.out.println("2. See average rating of food trucks");
 			System.out.println("3. Display highest rated food truck");
 			System.out.println("4. Quit the program");
+			System.out.println("**********************************");
 			int selection = kb.nextInt();
 			
+			if(selection == 4) {
+				System.out.println("Have a nice day!");
+				menu = false;
+			}
+				
 			if(selection == 1) {
 				for(int i =0; i < foodTrucks.length; i++) {
 					if(foodTrucks[i]== null) {
@@ -55,20 +64,51 @@ public class FoodTruckApp {
 			}
 			
 			if(selection == 2) {
+				int count= 0;
+				int sum = 0;
+				int avg = 0;
+				for(int i = 0; i < foodTrucks.length; i++) {
+					if (foodTrucks[i] == null) {
+						break;
+					}
+					else {
+						
+						sum = sum + foodTrucks[i].getRating();
+						count++;
+						
+					}
+					avg = sum /count;
+				}
+				System.out.println("The average rating of the foodtrucks is: " + avg );
 				
 			}
 			
+			if (selection == 3) {
+			int count= 0;
+			int max = foodTrucks[0].getRating();	
+			for(int i = 0; i < foodTrucks.length; i++) {
+				
+				if (foodTrucks[i] == null) {
+					break;
+				}
+				
+				else if  (max < foodTrucks[i].getRating()) { 
+			        max = foodTrucks[i].getRating();
+			        count= i;
+			      }
+				
+			}
+			
+				System.out.println("The highest rated truck is " + foodTrucks[count].toString());
+
+					 			
+			
+
 			
 			
 			
 			
 	}		
 }
-		
-		 
-				
-		
-		 
-	
-
-
+}
+}
